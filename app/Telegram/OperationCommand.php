f674c6135aca428781abfa2d802cc71293c58e9f
nawar16 <nawarmk98@gmail.com>
2021-05-02 02:20:58 +0300

    command

--- app/Telegram/OperationCommand.php
+++ app/Telegram/OperationCommand.php
@@ -17,22 +17,43 @@ class OperationCommand extends Command
      */
     public function handle()
     {
-        $this->replyWithMessage(['text' => trans('telegram.operation')]);
-        $keyboard = [
-            ['/one', '/two', '/three', '/example']
-        ];
+       // $this->replyWithMessage(['text' => trans('telegram.operation')]);
+        $update = $this->telegram->getWebhookUpdate();
+        $keyboard = Keyboard::make()
+        ->inline()
+        ->row(
+            Keyboard::inlineButton(['text' => 'One', 'callback_data' => '/One']),
+            Keyboard::inlineButton(['text' => 'Two', 'callback_data' => '/Two']),
+            Keyboard::inlineButton(['text' => 'Three', 'callback_data' => '/Three'])
+        );
+
+        Log::info('handle');
+        Log::info($update);
+
+        if ($update->isType('callback_query')) {
+            Log::info('...is callback');
+            $query = $update->getCallbackQuery();
+            $data  = $query->getData();
+            $chid = $query->getFrom()->getId();
         
-        $reply_markup = $this->getTelegram()->replyKeyboardMarkup([
-            'keyboard' => $keyboard, 
-            'resize_keyboard' => true, 
-            'one_time_keyboard' => true,
-            'hide_keyboard'=> true
-        ]);
-        
-        $response = $this->getTelegram()->sendMessage([
-            'chat_id' => 'CHAT_ID', 
-            'text' => 'Hello World', 
-            'reply_markup' => $reply_markup
-        ]);
+            $this->replyWithMessage([
+                'chat_id' => $chid,
+                'text' => 'Here is the callback: ' . $data,
+                'reply_markup' => $keyboard
+            ]);
+
+        } else {
+
+            Log::info('show keyboard...');
+
+            $chat_id = $update["message"]["chat"]["id"];
+
+            $response = $this->replyWithMessage([
+                'chat_id' => $chat_id,
+                'text' => trans('telegram.operation'),
+                'reply_markup' => $keyboard
+            ]);
+        }
+        //$this->triggerCommand('operation');
     }
 }
