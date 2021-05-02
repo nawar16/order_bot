7e4568abe9b6f82fb374dac829122c9967d94d43
nawar16 <nawarmk98@gmail.com>
2021-05-02 13:25:30 +0300

    upload

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -18,7 +18,7 @@ class TelegramController extends Controller
     protected $chat_id;
     public function __construct(){
         //Telegram::setTimeout(3000);
-        $this->telegram = new Api(env('TELEGRAM_BOT_TOKEN'), true);
+        $this->telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
         $lang =  Setting::first()->locale;
         \Session::put('lang', $lang);
         $this->middleware("Locale");
