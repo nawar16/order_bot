bc25b901850a4b14f1278b9fb9a7fcc6992f2bb6
nawar16 <nawarmk98@gmail.com>
2021-05-03 03:26:13 +0300

    bot

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     protected $table = 'settings';
-    protected $fillable = ['locale'];
+    protected $fillable = ['locale', 'chat_id'];
 }
